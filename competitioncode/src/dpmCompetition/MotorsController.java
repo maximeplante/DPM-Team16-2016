b5430a01cfa61c52642c4c78acb9bb802555875d
Maxime Plante <maxime.pl@outlook.com>
2016-11-05 12:44:28 -0400

    Initial commit of MotorsController

---
+++ competitioncode/src/dpmCompetition/MotorsController.java
@@ -0,0 +1,41 @@
+package dpmCompetition;
+
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+
+public class MotorsController {
+	
+	private final String leftWheelMotorPort = "A";
+	private final String rightWheelMotorPort = "B";
+	private final String doorMotorPort = "C";
+	private final String clawMotorPort = "D";
+	
+	private final EV3LargeRegulatedMotor leftWheelMotor;
+	private final EV3LargeRegulatedMotor rightWheelMotor;
+	private final EV3LargeRegulatedMotor doorMotor;
+	private final EV3LargeRegulatedMotor clawMotor;
+	
+	MotorsController() {
+		leftWheelMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort(leftWheelMotorPort));
+		rightWheelMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort(rightWheelMotorPort));
+		doorMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort(doorMotorPort));
+		clawMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort(clawMotorPort));
+	}
+	
+	public EV3LargeRegulatedMotor getLeftWheelMotor() {
+		return leftWheelMotor;
+	}
+	
+	public EV3LargeRegulatedMotor getRightWheelMotor() {
+		return rightWheelMotor;
+	}
+
+	public EV3LargeRegulatedMotor getDoorMotor() {
+		return doorMotor;
+	}
+
+	public EV3LargeRegulatedMotor getClawMotor() {
+		return clawMotor;
+	}
+	
+}
