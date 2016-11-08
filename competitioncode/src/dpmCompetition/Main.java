6ba7395e25bd9b0795b6d120b05bf8a834a263c3
Maxime Plante <maxime.pl@outlook.com>
2016-11-07 21:51:41 -0500

    Basic implementation of Main
    There is more to come. It will change gradually while the different
    pieces of the code are put together.

--- competitioncode/src/dpmCompetition/Main.java
+++ competitioncode/src/dpmCompetition/Main.java
@@ -1,11 +1,41 @@
 package dpmCompetition;
 
+import lejos.hardware.Button;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+
 public class Main {
+
+	static private final String upperUsSensorPort = "A";
 
 	public static void main(String[] args) {
 		
+		// Motos provider
 		MotorsController motorsController = new MotorsController();
 		
+		// Odometry
+		OdometerCorrection odometerCorrection = new OdometerCorrection();
+		Odometer odometer = new Odometer(motorsController, 25, false);
+		
+		// Sensors
+		EV3UltrasonicSensor upperUs = new EV3UltrasonicSensor(LocalEV3.get().getPort(upperUsSensorPort));
+		UsPoller upperUsPoller = new UsPoller(upperUs);
+		
+		// Localization
+		Localizer localizer = new Localizer(odometer, upperUsPoller, null, motorsController);
+		
+		// Competition brain
+		Brain brain = new Brain();
+
+		// Starting the threads
+		odometerCorrection.start();
+		upperUsPoller.start();
+		odometer.start();
+		brain.start();
+		
+		// Wait for the user to quit
+		while (Button.waitForAnyPress() != Button.ID_ESCAPE);
+		System.exit(0);
 	}
 
 }
