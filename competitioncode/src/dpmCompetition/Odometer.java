dbe2a092d03d6aa9f3791c46aee8e208f15b00d2
vivekster12 <vivek.gidla@mail.mcgill.ca>
2016-11-07 19:17:55 -0500

    Odometer (so far)

--- competitioncode/src/dpmCompetition/Odometer.java
+++ competitioncode/src/dpmCompetition/Odometer.java
@@ -1,5 +1,167 @@
 package dpmCompetition;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.utility.Timer;
+import lejos.utility.TimerListener;
 
-public class Odometer {
+public class Odometer implements TimerListener{
+	private Timer timer;
+	private EV3LargeRegulatedMotor leftMotor, rightMotor;
+	private final int DEFAULT_TIMEOUT_PERIOD = 20;
+	private double leftRadius, rightRadius, width;
+	private double x, y, theta;
+	private double[] oldDH, dDH;
+	// motor controller
+	private MotorsController MotorsController;
+	
+	// constructor
+	public Odometer (MotorsController motorsController, int INTERVAL, boolean autostart) {
+		this.MotorsController=motorsController;
+		this.leftMotor = motorsController.getLeftWheelMotor();
+		this.rightMotor = motorsController.getRightWheelMotor();
+				
+		// default values, modify for your robot
+		this.rightRadius = 2.15;
+		this.leftRadius = 2.15;
+		this.width = 12.0;
+				
+		this.x = 0.0;
+		this.y = 0.0;
+		this.theta = 90.0;
+		this.oldDH = new double[2];
+		this.dDH = new double[2];
+		
+		if (autostart) {
+			// if the timeout interval is given as <= 0, default to 20ms timeout 
+			this.timer = new Timer((INTERVAL <= 0) ? INTERVAL : DEFAULT_TIMEOUT_PERIOD, this);
+			this.timer.start();
+			} else
+				this.timer = null;
+		}
+	
+	// functions to start/stop the timerlistener
+	public void stop() {
+		if (this.timer != null)
+			this.timer.stop();
+	}
+	public void start() {
+		if (this.timer != null)
+			this.timer.start();
+	}
+	
+	/*
+	 * Calculates displacement and heading as title suggests
+	 */
+	private void getDisplacementAndHeading(double[] data) {
+		int leftTacho, rightTacho;
+		leftTacho = leftMotor.getTachoCount();
+		rightTacho = rightMotor.getTachoCount();
 
+		data[0] = (leftTacho *leftRadius + rightTacho * rightRadius) * Math.PI / 360.0;
+		data[1] = (rightTacho * rightRadius - leftTacho * leftRadius) / width;
+	}
+	
+	/*
+	 * Recompute the odometer values using the displacement and heading changes
+	 */
+	public void timedOut() {
+		this.getDisplacementAndHeading(dDH);
+		dDH[0] -= oldDH[0];
+		dDH[1] -= oldDH[1];
+
+		// update the position in a critical region
+		synchronized (this) {
+			theta += dDH[1];
+			theta = fixDegAngle(theta);
+
+			x += dDH[0] * Math.cos(Math.toRadians(theta));
+			y += dDH[0] * Math.sin(Math.toRadians(theta));
+		}
+
+		oldDH[0] += dDH[0];
+		oldDH[1] += dDH[1];
+	}
+	
+	// return X value
+		public double getX() {
+			synchronized (this) {
+				return x;
+			}
+		}
+
+		// return Y value
+		public double getY() {
+			synchronized (this) {
+				return y;
+			}
+		}
+
+		// return theta value
+		public double getAng() {
+			synchronized (this) {
+				return theta;
+			}
+		}
+
+		// set x,y,theta
+		public void setPosition(double[] position, boolean[] update) {
+			synchronized (this) {
+				if (update[0])
+					x = position[0];
+				if (update[1])
+					y = position[1];
+				if (update[2])
+					theta = position[2];
+			}
+		}
+
+		// return x,y,theta
+		public void getPosition(double[] position) {
+			synchronized (this) {
+				position[0] = x;
+				position[1] = y;
+				position[2] = theta;
+			}
+		}
+
+		public double[] getPosition() {
+			synchronized (this) {
+				return new double[] { x, y, theta };
+			}
+		}
+		
+		// accessors to motors
+		public EV3LargeRegulatedMotor [] getMotors() {
+			return new EV3LargeRegulatedMotor[] {this.leftMotor, this.rightMotor};
+		}
+		public EV3LargeRegulatedMotor getLeftMotor() {
+			return this.leftMotor;
+		}
+		public EV3LargeRegulatedMotor getRightMotor() {
+			return this.rightMotor;
+		}
+
+		// static 'helper' methods
+		public static double fixDegAngle(double angle) {
+			if (angle < 0.0)
+				angle = 360.0 + (angle % 360.0);
+
+			return angle % 360.0;
+		}
+
+		public static double minimumAngleFromTo(double a, double b) {
+			double d = fixDegAngle(b - a);
+
+			if (d < 180.0)
+				return d;
+			else
+				return d - 360.0;
+		}
+
+		public double getLeftRadius() {
+			return leftRadius;
+		}
+
+		public double getWidth() {
+			return width;
+		}
 }
