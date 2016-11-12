41a93d8f4444361bf9813f49235e64cdb44e7d51
gtsutsumi <gen.tsutsumi@mail.mcgill.ca>
2016-11-12 13:10:52 -0500

    set odometer to right coord (X1-4) in brain
    i have made the localizer go to its relative (0,0) first and then
    adjusted the coordinates and angle in respect to the absolute (0,0,)
    after navgiating to its relative (0,0)

--- competitioncode/src/dpmCompetition/Brain.java
+++ competitioncode/src/dpmCompetition/Brain.java
@@ -3,15 +3,48 @@ package dpmCompetition;
 public class Brain extends Thread {
 	
 	private Localizer localizer;
+	private Driver driver;
+	private Navigation navigation;
+	private Odometer odo;
 	
-	Brain(Localizer localizer) {
+	Brain(Localizer localizer, Driver driver, Navigation navigation, Odometer odo) {
 		this.localizer = localizer;
+		this.driver = driver;
+		this.navigation = navigation;
+		this.odo = odo;
 	}
 	
 	public void run() {
+		int startXcoord;
+		int startYcoord;
+		int startAng;
 		
 		localizer.localize();
+		driver.travelTo(0,0);
+		navigation.turnTo(0);
+		if (X1){
+			startXcoord = 0;
+			startYcoord = 0;
+			startAng = 0;
+		}
+		else if (X2) {
+			startXcoord = 11;
+			startYcoord = 0;
+			startAng = 1;
+		}
+		else if (X3) {
+			startXcoord = 11;
+			startYcoord = 11;
+			startAng = 2;
+		}
+		else {
+			startXcoord = 0;
+			startYcoord = 11;
+			startAng = 3;
+		}
+		odo.setPosition(new double [] {startXcoord*Main.TILE_LENGTH, startYcoord*Main.TILE_LENGTH,startAng*90}, new boolean [] {true, true, true});
 		
 	}
+	
 	
 }
