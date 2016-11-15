e0ffbef974d5333618fe5fe5abd09e22e4adc8e9
Maxime Plante <maxime.pl@outlook.com>
2016-11-14 19:31:13 -0500

    Removed some unused code + small improvements

--- competitioncode/src/dpmCompetition/Driver.java
+++ competitioncode/src/dpmCompetition/Driver.java
@@ -1,16 +1,11 @@
 package dpmCompetition;
 
-import lejos.hardware.motor.EV3LargeRegulatedMotor;
-
 public class Driver extends Thread{
 	private double targetX;
 	private double targetY;
 	
 	private Odometer odometer;
 	private Navigation navigation;
-	
-	private EV3LargeRegulatedMotor leftMotor;
-	private EV3LargeRegulatedMotor rightMotor;
 	
 	private static final double acceptableError = 5;
 	private final int DEFAULT_TIMEOUT_PERIOD = 20;
@@ -36,7 +31,7 @@ public class Driver extends Thread{
 		targetY = y;
 		isNavigating = true;
 			
-		}
+	}
 	
 	public void run() {
 			while (true) {
@@ -56,7 +51,7 @@ public class Driver extends Thread{
 				
 				// Turn only if the minimal angle to turn is larger than 50 degrees (in any direction)
 				// Prevents the plant from doing a lot of small turns that could induce more error in the odometer.
-				if (navigation.minimalAngleDifference(odometer.getTheta(), angle) > acceptableError || navigation.minimalAngleDifference(odometer.getTheta(), angle) < -acceptableError) {
+				if (Navigation.minimalAngleDifference(odometer.getTheta(), angle) > acceptableError || Navigation.minimalAngleDifference(odometer.getTheta(), angle) < -acceptableError) {
 					navigation.turnTo(angle);
 				}
 				
