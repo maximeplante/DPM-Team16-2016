fae2258a1d07d5542c8e5c92874d0b8766caecf3
Maxime Plante <maxime.pl@outlook.com>
2016-11-15 17:18:50 -0500

    Documented Display

--- competitioncode/src/dpmCompetition/Display.java
+++ competitioncode/src/dpmCompetition/Display.java
@@ -4,15 +4,27 @@ import lejos.hardware.lcd.TextLCD;
 
 public class Display extends Thread {
 	
+	/** The delay in ms between the screen refreshes */
 	private static final long DISPLAY_PERIOD = 250;
+	
+	/** Reference to the EV3's LCD screen */
 	private TextLCD lcd;
 	
+	/** The lines do display during the next screen refresh */
 	static private String[] lines = {"", "", "", "", ""};
 	
+	/**
+	 * Constructor
+	 * 
+	 * @param lcd reference to EV3's LCD screen
+	 */
 	Display(TextLCD lcd) {
 		this.lcd = lcd;
 	}
 	
+	/**
+	 * The thread code.
+	 */
 	public void run() {
 		lcd.clear();
 		
@@ -32,10 +44,28 @@ public class Display extends Thread {
 		}
 	}
 	
+	/**
+	 * Sets a message to display on a certain lines of the display.
+	 * 
+	 * The message will appear during the next screen refresh. It is a static
+	 * method, so it can be called from anywhere in the code.
+	 * 
+	 * @param msg the message to show
+	 * @param line the line of the screen on which the message should be printed
+	 */
 	static public void print(String msg, int line){
 		lines[line % lines.length] = msg;
 	}
 	
+	/**
+	 * Sets a double to display on a certain lines of the display.
+	 * 
+	 * The double will appear during the next screen refresh. It is a static
+	 * method, so it can be called from anywhere in the code.
+	 * 
+	 * @param msg the double to show
+	 * @param line the line of the screen on which the double should be printed
+	 */
 	static public void print(double msg, int line) {
 		print(String.valueOf(msg), line);
 	}
