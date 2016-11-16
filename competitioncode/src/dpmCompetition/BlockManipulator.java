5ab906bfd099cf18c053d456f625cf8ca2c09296
Judy Zhu <judyyes10@gmail.com>
2016-11-15 22:45:51 -0500

    Add javadoc to blockManipulator

--- competitioncode/src/dpmCompetition/BlockManipulator.java
+++ competitioncode/src/dpmCompetition/BlockManipulator.java
@@ -2,32 +2,66 @@ package dpmCompetition;
 
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 
+/**
+ * 
+ * Provides the robot the capabilities of catching a block 
+ * and releasing the tower that it has carried.
+ * 
+ */
 public class BlockManipulator {
+	
+	/** Reference to the claw's motor */
 	private EV3LargeRegulatedMotor clawMotor;
+	
+	/** Reference to the door's motor */
 	private EV3LargeRegulatedMotor doorMotor;
+	
+	/** Reference to the navigation used by the robot */
 	private Navigation navigation;
-	// degree needs to rotate the claw
-	private int rotateDegree = 350;
-	// claw motor rotate speed
+	
+	/** Distance needed for the robot to go backward in order to drop the claw */
+	private int backward = 15;
+	
+	/** Degree needed to be rotated to drop the claw */
+	private int clawDegree = 350;
+	
+	/** Claw's and door's motor rotate speed */
 	private int turnSpeed = 20;
-	// degree needs to open the door
+	
+	/** Degree needed to be rotated in order to open the door */
 	private int doorDegree = 90;
-	// 
+	
+	/** Distance needed for the robot to go forward to release the tower */
 	private int forward = 30;
 	
+	/**
+	 * 
+	 * Constructor
+	 * 
+	 * @param motorsController provides a way to access the motors
+	 * @param navigation controls robot's motion
+	 */
 	public BlockManipulator(MotorsController motorsController, Navigation navigation) {
 		this.clawMotor = motorsController.getClawMotor();
 		this.doorMotor = motorsController.getDoorMotor();
 		this.navigation = navigation;
 	}
+	
+	/**
+	 * The robot goes backward, drops the claw, goes forward, catches the block 
+	 * and dumps it into the back bin.
+	 */
 	public void catchBlock(){
-		navigation.goForward(-10);
+		navigation.goForward(-backward);
 		clawMotor.setSpeed(turnSpeed);
-		clawMotor.rotate(-rotateDegree);
-		navigation.goForward(10);
-		clawMotor.rotate(rotateDegree);
+		clawMotor.rotate(-clawDegree);
+		navigation.goForward(backward);
+		clawMotor.rotate(clawDegree);
 	}
 	
+	/**
+	 * The robot opens its door, goes forward, releases its tower and closes its door. 
+	 */
 	public void releaseTower(){
 		doorMotor.setSpeed(turnSpeed);
 		doorMotor.rotate(-doorDegree);
