622fe888c3495a5a0be942532524adb25c3ce949
Maxime Plante <maxime.pl@outlook.com>
2016-11-16 23:46:49 -0500

    Changed Brain to support basic beta demo
    Still needs to go to the green zone

--- competitioncode/src/dpmCompetition/Brain.java
+++ competitioncode/src/dpmCompetition/Brain.java
@@ -9,13 +9,15 @@ public class Brain extends Thread {
 	private Navigation navigation;
 	private Odometer odo;
 	private BlockManipulator blockManipulator;
+	private AreaScanner areaScanner;
 	
-	Brain(Localizer localizer, Driver driver, Navigation navigation, Odometer odo, BlockManipulator blockManipulator) {
+	Brain(Localizer localizer, Driver driver, Navigation navigation, Odometer odo, BlockManipulator blockManipulator, AreaScanner areaScanner) {
 		this.localizer = localizer;
 		this.driver = driver;
 		this.navigation = navigation;
 		this.odo = odo;
 		this.blockManipulator = blockManipulator;
+		this.areaScanner = areaScanner;
 	}
 	
 	public void run() {
@@ -26,37 +28,33 @@ public class Brain extends Thread {
 		
 		localizer.localize();
 		driver.travelTo(0,0);
-		/*while (driver.isNavigating()) {
-			try {
-				sleep(100);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		driver.travelTo(30,30);*/
-		//navigation.turnTo(0);
-//		if (X1){
+		navigation.turnTo(0);
+		//if (X1){
 			startXcoord = 0;
 			startYcoord = 0;
 			startAng = 0;
-//		}
-//		else if (X2) {
-//			startXcoord = 11;
-//			startYcoord = 0;
-//			startAng = 1;
-//		}
-//		else if (X3) {
-//			startXcoord = 11;
-//			startYcoord = 11;
-//			startAng = 2;
-//		}
-//		else {
-//			startXcoord = 0;
-//			startYcoord = 11;
-//			startAng = 3;
-//		}
-		//odo.setPosition(new double [] {startXcoord*Main.TILE_LENGTH, startYcoord*Main.TILE_LENGTH,startAng*90}, new boolean [] {true, true, true});
-		Sound.beep();
+		/*}
+		else if (X2) {
+			startXcoord = 11;
+			startYcoord = 0;
+			startAng = 1;
+		}
+		else if (X3) {
+			startXcoord = 11;
+			startYcoord = 11;
+			startAng = 2;
+		}
+		else {
+			startXcoord = 0;
+			startYcoord = 11;
+			startAng = 3;
+		}*/
+		odo.setPosition(new double [] {startXcoord*Main.TILE_LENGTH, startYcoord*Main.TILE_LENGTH,startAng*90}, new boolean [] {true, true, true});
+		
+		Coordinate[] objects = areaScanner.findCloseObjects();
+		driver.travelTo(objects[0].x, objects[0].y);
+		
+		blockManipulator.catchBlock();
+		
 	}
 }
