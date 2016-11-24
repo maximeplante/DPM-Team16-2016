8262e1edff69dfa546dba5bfae94fd154da47fc5
Judy Zhu <judyyes10@gmail.com>
2016-11-24 16:14:57 -0500

    added javadoc to block

--- competitioncode/src/dpmCompetition/Block.java
+++ competitioncode/src/dpmCompetition/Block.java
@@ -1,11 +1,17 @@
 package dpmCompetition;
 
+/**
+ *
+ */
 public class Block {
 
 	public Coordinate center;
 	public Coordinate left;
 	public Coordinate right;
 	
+	/**
+	 * Constructor
+	 */
 	Block() {
 		center = new Coordinate();
 		left = new Coordinate();
